fbb3f1dce89b0626e6fdf4b0c6b32f67e9ae20c9
vigneshrengas <67945218+vigneshrengas@users.noreply.github.com>
2020-10-01 13:23:15 +0530

    initial commit
    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,128 @@
 import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+
+const StyledTableCell = withStyles((theme) => ({
+	head: {
+	  backgroundColor: theme.palette.common.black,
+	  color: theme.palette.common.white,
+	},
+	body: {
+	  fontSize: 14,
+	},
+  }))(TableCell);
+  
+
+  const StyledTableRow = withStyles((theme) => ({
+	root: {
+	  '&:nth-of-type(odd)': {
+		backgroundColor: theme.palette.action.hover,
+	  },
+	},
+  }))(TableRow);
+
+const useStyles = makeStyles({
+	table: {
+	  minWidth: 700,
+	},
+  });
+  
+  function createData(customerId, customerName, customerBank, currency) {
+	return { customerId, customerName, customerBank, currency };
+  }
+
+  
+  const rows = [
+	createData(1, 'Ragul', 'ABC Bank', 'INR'),
+	createData(2, 'Vignesh', 'XYZ Bank', 'USD'),
+	createData(3, 'Ragul', 'QWE Bank', 'EUR'),
+	createData(4, 'Vignesh', 'ABC Bank', 'INR'),
+	createData(5, 'Ragul', 'QWE Bank', 'USD'),
+  ];
+
+  
+  export default function BasicTable() {
+	const classes = useStyles();
+	
+	//const [name, setRow] = React.useState('check');
+	const [custName, setName] = React.useState('');
+
+	// const onclickFunc = (event) =>{
+	// 	setRow(event.target.value);
+	// 	 return console.log(event.target.value, name);
+	//  }
+
+	const edit = (data) => { 
+		// Do whatever you want
+		return console.log( data);
+
+	}
+	const handleChange = (event) => {
+		console.log(event.target.value,  custName);
+	  setName(event.target.value);
+	  rows.filter( row => {
+		 if(row.customerName.toLowerCase().includes(event.target.value)) {
+			 console.log('row-->', row);
+			 return row;
+		 }
+		 return row;
+	  })
+	};
+
+	return (
+		<>
+		<form>
+            <FormControl>
+            <InputLabel>Name</InputLabel>
+          <Input value={custName} onChange={handleChange} /> 
+            </FormControl>
+        </form>
+
+	  <TableContainer component={Paper}>
+		<Table className={classes.table} aria-label="customized table">
+		  <TableHead>
+		  <TableRow>
+            <StyledTableCell align="right">Customer Id</StyledTableCell>
+            <StyledTableCell align="right">Customer Name&nbsp;(g)</StyledTableCell>
+            <StyledTableCell align="right">Customer Bank&nbsp;(g)</StyledTableCell>
+            <StyledTableCell align="right">Currency&nbsp;(g)</StyledTableCell>
+			<StyledTableCell align="left">Button&nbsp;(g)</StyledTableCell>
+          </TableRow>
+		  </TableHead>
+		  <TableBody>
+			{rows.map((row) => (
+			  <StyledTableRow key={row.customerId}>
+              <StyledTableCell component="th" scope="row">
+                {row.customerId}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.customerName}</StyledTableCell>
+              <StyledTableCell align="right">{row.customerBank}</StyledTableCell>
+              <StyledTableCell align="right">{row.currency}</StyledTableCell>
+			<StyledTableCell ><Button onClick={() => edit(row)} variant="outlined" color="primary">Click</Button></StyledTableCell>
+			  </StyledTableRow>
+			))}
+		  </TableBody>
+		</Table>
+	  </TableContainer>
+	  </>
+	);
+  }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BasicTable  />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
